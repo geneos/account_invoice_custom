73593e5d46854aaedb724fc2337377345912e5ba
Fernando Recci <reccifernando@gmail.com>
2023-09-08 14:49:34 -0300

    commit inicial

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Módulo Custom GEA",
+    'version': '11.0.0.0.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'GENEOS Cooperativa Ltda.',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'l10n_ar_account',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'view/account_invoice_view.xml',
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ models/account_invoice.py
@@ -0,0 +1,20 @@
+from odoo import models, fields, api, _
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    mensaje_activo = fields.Boolean(string='Activo')
+    cotizacion_dolar = fields.Monetary(string='Cotizacion', currency_field='currency_id')
+    total_dolares = fields.Monetary(string='Total Dolares', currency_field='currency_id')
+    mensaje_invoice = fields.Text(string='Mensaje Invoice', compute='_compute_mensaje_invoice')
+
+
+    @api.one
+    @api.depends('currency_id')
+    def _compute_mensaje_invoice(self):
+        self.mensaje_invoice = '''La presente factura fue emitida a una cotización dólar Estadounidenses de $ %.2f
+Siendo el total de la misma la cantidad de U$S  %.2f
+La misma debe ser cancelada dentro de las 72hs hábiles fecha de factura según condición de venta pactada, caso contrario será refacturada a una nueva cotización vigente.
+        ''' % (self.cotizacion_dolar, self.total_dolares)
+
