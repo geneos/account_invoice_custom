4363b6e64ecca605299cb976e448c5b89568bd2f
Fernando Recci <reccifernando@gmail.com>
2023-10-26 09:22:04 -0300

    cambio leyenda

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -13,7 +13,7 @@ class AccountInvoice(models.Model):
     @api.one
     @api.depends('currency_id')
     def _compute_mensaje_invoice(self):
-        self.mensaje_invoice = '''La presente factura fue emitida a una cotización dólar Estadounidenses de $ %.2f
+        self.mensaje_invoice = '''La presente factura fue emitida a una cotización dólar TARJETA de $ %.2f
 Siendo el total de la misma la cantidad de U$S  %.2f
 La misma debe ser cancelada dentro de las 72hs hábiles fecha de factura según condición de venta pactada, caso contrario será refacturada a una nueva cotización vigente.
         ''' % (self.cotizacion_dolar, self.total_dolares)
